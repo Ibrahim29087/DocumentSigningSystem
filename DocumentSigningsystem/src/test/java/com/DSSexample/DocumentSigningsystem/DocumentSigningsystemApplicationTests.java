d479445a0186c2826fef85a163e2868d4e88453a
Muhammad Ibrahim Raza <razaibrahim101@gmail.com>
2026-06-18 23:43:46 +0500

    fix: add test profile with application-test.properties

--- DocumentSigningsystem/src/test/java/com/DSSexample/DocumentSigningsystem/DocumentSigningsystemApplicationTests.java
+++ DocumentSigningsystem/src/test/java/com/DSSexample/DocumentSigningsystem/DocumentSigningsystemApplicationTests.java
@@ -2,12 +2,13 @@ package com.DSSexample.DocumentSigningsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DocumentSigningsystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
